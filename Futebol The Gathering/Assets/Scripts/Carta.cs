8a74ea156b18a62291f072326992541263ddb32c
RenanFarias <Renan.ii@hotmail.com>
2016-02-23 16:33:18 -0300

    Criação da Classe PErsonagem

---
+++ Futebol The Gathering/Assets/Scripts/Carta.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Carta : MonoBehaviour {
+
+	public class Personagem{
+		public string name;
+		public float chute;
+		public float defesa;
